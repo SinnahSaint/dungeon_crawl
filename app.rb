7bfab34f43c0cdc230b9cca2e9b2f7762e01a0f4
SinnahSaint <sinnah.saint@gmail.com>
2020-07-19 10:12:23 -0400

    Made a tiny map of 3 rooms
    Used to test init with the only 3 templates that could flow (a noEnc+ inv, noEnc+noInv,, and fire) No errors on innit like this.(which is awesome!)

--- app.rb
+++ app.rb
@@ -39,10 +39,12 @@ class Application
       n: Template.new(description: "literally boring nothing room"),
     }
 
-    @map = [
-      [Room.new(@lay[:es], @temp[:f]), Room.new(@lay[:esw], @temp[:n]), Room.new(@lay[:w], @temp[:a])],
-      [Room.new(@lay[:ns], @temp[:n]), Room.new(@lay[:n], @temp[:g]),   Room.new(@lay[:s], @temp[:c])],
-      [Room.new(@lay[:ne], @temp[:j]), Room.new(@lay[:esw], @temp[:n]), Room.new(@lay[:nw], @temp[:a])],
+    @map = [Room.new(@lay[:es], @temp[:f]), Room.new(@lay[:esw], @temp[:g]), Room.new(@lay[:e], @temp[:n])
+    #fake map to test init
+    
+      # [Room.new(@lay[:es], @temp[:f]), Room.new(@lay[:esw], @temp[:n]), Room.new(@lay[:w], @temp[:a])],
+  #     [Room.new(@lay[:ns], @temp[:n]), Room.new(@lay[:n], @temp[:g]),   Room.new(@lay[:s], @temp[:c])],
+  #  [Room.new(@lay[:ne], @temp[:j]), Room.new(@lay[:esw], @temp[:n]), Room.new(@lay[:nw], @temp[:a])],
     ]
     
   end
