4eebf11c96fc8bc5e9fb3024be24d6363419e9b2
SinnahSaint <sinnah.saint@gmail.com>
2020-07-19 16:26:04 -0400

    oops, missed out on the ice room

--- app.rb
+++ app.rb
@@ -41,7 +41,7 @@ class Application
     @map = [
       [Room.new(@lay[:es], @temp[:f]), Room.new(@lay[:esw], @temp[:n]), Room.new(@lay[:w], @temp[:a])],
       [Room.new(@lay[:ns], @temp[:n]), Room.new(@lay[:n], @temp[:g]),   Room.new(@lay[:s], @temp[:c])],
-      [Room.new(@lay[:ne], @temp[:j]), Room.new(@lay[:esw], @temp[:n]), Room.new(@lay[:nw], @temp[:a])]
+      [Room.new(@lay[:ne], @temp[:j]), Room.new(@lay[:esw], @temp[:n]), Room.new(@lay[:nw], @temp[:i])]
     ]
     
     current_room
