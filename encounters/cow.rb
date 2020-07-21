a786764d3aeb420187b581c3d2038a093df96b09
SinnahSaint <sinnah.saint@gmail.com>
2020-07-21 14:31:38 -0400

    Cow can be milked twice only

--- encounters/cow.rb
+++ encounters/cow.rb
@@ -2,6 +2,7 @@ class Cow
   attr_reader :blocking, :inventory
 
   def initialize
+    @milked = false
     @has_milk = true
     @blocking = false
   end
@@ -9,23 +10,39 @@ class Cow
   def remove_item(item)
     @inventory.delete(item)
   end
-  
-  def handle_command(cmdstr, avatar)
-    if cmdstr == "milk cow" || cmdstr == "milk"
+
+  def milk_bessy(avatar)
+    if @has_milk && !@milked
+      puts "You get some milk."
+      @milked = true
+      avatar.inventory << "milk"
+      return true
+    elsif @has_milk && @milked
+      puts "You get the rest of the milk."
       @has_milk = false
       avatar.inventory << "milk"
       return true
     else
-      puts "Bessy looks at you oddly."
-      return false
+      puts "Bessy is not going to let you near her again today."
+      return true
     end
+  end
+  
+  def handle_command(cmdstr, avatar)
+    if cmdstr == "milk cow" || cmdstr == "milk"
+      milk_bessy(avatar)
+    else
+      puts "Bessy looks at you oddly."
+      return false 
+    end
+  end
   
   def hint
     puts "Cows produce a LOT of milk each day."
   end
   
   def state
-    if @has_milk
+    if @has_milk 
       "There is a cow standing by a milking stool. She looks really uncomfortable."
     else
       "The cow is happy."
