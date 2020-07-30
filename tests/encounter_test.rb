35f1620a3ffbde01d48ed8714b202c273809504f
SinnahSaint <sinnah.saint@gmail.com>
2020-07-30 14:13:35 -0400

    Applied basic Test::Unit structure

---
+++ tests/encounter_test.rb
@@ -0,0 +1,13 @@
+require 'test/unit'
+
+class EncounterTest < Test::Unit::TestCase
+  # def setup
+  # end
+
+  # def teardown
+  # end
+
+  def test_fail
+    assert(false, 'Assertion was false.')
+  end
+end
