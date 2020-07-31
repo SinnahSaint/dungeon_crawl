21bf0de4918fefe700da19383ab0c359ac7320a4
SinnahSaint <sinnah.saint@gmail.com>
2020-07-30 20:05:50 -0400

    Room tests

--- tests/room_test.rb
+++ tests/room_test.rb
@@ -1,13 +1,35 @@
 require 'test/unit'
+require 'ostruct'
+require_relative '../room.rb'
 
 class RoomTest < Test::Unit::TestCase
-  # def setup
-  # end
+  def setup
+  @template = OpenStruct.new({
+    build_encounter: "encounter built", 
+    description: "template description",
+    inventory: %w[item another],
+  })
+  
+  @room = Room.new("layout", @template)
+  end
+
+  def test_room_creation    
+    assert_equal @room.lay, "layout"
+    assert_equal @room.enc, "encounter built"
+    assert_equal @room.inventory, %w[item another]
+  end
+  
+  
+  def test_room_description
+    assert_equal @room.description, "template description"
+  end
+
+
+  def test_item_delete
+    @room.remove_item("item")
+    assert_not_equal @room.inventory, @template.inventory
+  end
 
   # def teardown
-  # end
-
-  def test_
-    omit("there's no tests here")
-  end
+  # end  
 end
