bfd9c54fb59ca7a525ac6711195c4ea8673b100a
SinnahSaint <sinnah.saint@gmail.com>
2020-07-30 20:08:50 -0400

    Moved room description to make more sense.

--- room.rb
+++ room.rb
@@ -1,15 +1,11 @@
 class Room
-  attr_reader :lay, :inventory, :enc
+  attr_reader :lay, :inventory, :enc, :description
   
   def initialize(lay, temp)
     @lay = lay
-    @temp = temp
+    @description = temp.description
     @enc = temp.build_encounter
     @inventory = temp.inventory.dup
-  end
-  
-  def description
-    @temp.description
   end
   
   def remove_item(item)
--- tests/room_test.rb
+++ tests/room_test.rb
@@ -17,19 +17,11 @@ class RoomTest < Test::Unit::TestCase
     assert_equal @room.lay, "layout"
     assert_equal @room.enc, "encounter built"
     assert_equal @room.inventory, %w[item another]
-  end
-  
-  
-  def test_room_description
     assert_equal @room.description, "template description"
   end
-
 
   def test_item_delete
     @room.remove_item("item")
     assert_not_equal @room.inventory, @template.inventory
-  end
-
-  # def teardown
-  # end  
+  end 
 end
