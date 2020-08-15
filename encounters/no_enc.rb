a7511f8b68dbd9bd9a5571ffb7d405c35943465f
SinnahSaint <sinnah.saint@gmail.com>
2020-08-15 17:59:55 -0400

    room and encounter equality
    needed to be able to compare different instances to see if they're the same state

--- encounters/no_enc.rb
+++ encounters/no_enc.rb
@@ -6,6 +6,14 @@ class NoEnc
     @inventory = []
   end
   
+  def ==(other)
+    return false unless self.class == other.class
+    return false unless @blocking == other.blocking
+    return false unless @inventory == other.inventory
+    return false unless state == other.state
+    true
+  end
+  
   def remove_item(item)
     @inventory.delete(item)
   end
--- room.rb
+++ room.rb
@@ -1,6 +1,15 @@
 class Room
   attr_reader :lay, :inventory, :enc, :description
   
+  def ==(other)
+    return false unless other.is_a?(Room)
+    return false unless @description == other.description
+    return false unless @inventory.sort == other.inventory.sort
+    return false unless @lay == other.lay
+    return false unless @enc == other.enc
+    true
+  end
+  
   def initialize(lay, temp)
     @lay = lay
     @description = temp.description
