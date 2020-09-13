968fecebd7a4f01cac7c8012e7c8f8571f7a237a
SinnahSaint <sinnah.saint@gmail.com>
2020-09-13 10:21:57 -0400

    delete old templates

--- app/map.rb
+++ app/map.rb
@@ -5,18 +5,6 @@ end
 
 class Map
   attr_reader :level, :start, :win, :text
-    #
-  # TEMP = {
-  #   a: Template.new(encounter: ->{Avalanche.new}, inventory:["gemstone"], description: "A dusty room full of rubble. "),
-  #   c: Template.new(encounter: ->{Cow.new}, description: "A mostly empty room with straw on the floor. "),
-  #   f: Template.new(encounter: ->{Fire.new}, inventory: ["knife"], description: "A kitchen with a nice table. "),
-  #   i: Template.new(encounter: ->{Ice.new}, description: "This room is really cold for no good reason. "),
-  #   j: Template.new(encounter: ->{Jester.new}, description: "A throne room, with no one on the throne. "),
-  #   k: Template.new(encounter: ->{Killer.new}, description: "This room looks like you walked into a bandit's home office. "),
-  #   g: Template.new(inventory:["gold"], description: "A lovely room filled with gold. "),
-  #   n: Template.new(description: "A literally boring nothing room. "),
-  # }.freeze
-  #
 
   def initialize(level: nil, start: nil, win: nil, text: nil)
     @level = level
