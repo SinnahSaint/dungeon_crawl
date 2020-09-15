11237c1f991ece6ff15056b8b10ab107b61c46ef
SinnahSaint <sinnah.saint@gmail.com>
2020-09-15 19:34:42 -0400

    it still runs!

--- run.rb
+++ run.rb
@@ -1,5 +1,21 @@
 require_relative 'app/game.rb'
 
-Game.new.run
 
-#Game.new(map_file: './maps/in_and_out.yaml').run
+class App
+  
+  def run
+    Game.new.run
+    #Game.new(map_file: './maps/in_and_out.yaml').run
+  end
+  
+  
+end
+
+
+
+
+
+
+
+App.new.run
+
