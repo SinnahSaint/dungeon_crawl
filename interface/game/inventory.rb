0e25e110653ea1726b80531a7212894486b8decf
SinnahSaint <sinnah.saint@gmail.com>
2020-11-02 10:00:58 -0500

    bugs in inventory

--- interface/game/inventory.rb
+++ interface/game/inventory.rb
@@ -1,12 +1,20 @@
-def class Inventory
+class Inventory
+
   attr_accessor :loot, :equipment
 
   def initialize(loot:, equipment:)
     @loot = loot || []
-    @equipment = {head:,torso:,feet:,weapon:,trinket:,mood:,}.merge(equipment)
+    @equipment = {
+      head: nil, 
+      torso: nil, 
+      feet: nil, 
+      weapon: nil, 
+      trinket: nil, 
+      mood: nil, 
+      }.merge(equipment)
   end
 
-  def add_item(name:, type:"stackable",)
+  def add_item(name: nil, type:"stackable")
     @loot << Item.new(name: name, type: type,)
   end
 
@@ -21,15 +29,17 @@ def class Inventory
 
   def equip_item(item_name)
     gear = find_item_index_by_name(item_name)
-    slot = gear[type]
-
-    @equipment.merge{ slot => gear }
+    slot = gear.type
+ 
+    @equipment[slot.to_sym] = gear
 
     remove_item(item_name)
   end
 
-  def unequip_item(item_name)
-
+  def unequip_item(slot)
+    gear = equipment[slot]
+    @loot<<gear
+    @equipment[slot] = nil
   end
 
   def to_h
